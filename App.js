1a997f42f33244c109e7e4fbdec9ccaa23c32610
LMendoza70 <luis.mendoza@uthh.edu.mx>
2024-01-25 09:48:30 -0600

    25/01/24

--- App.js
+++ App.js
@@ -1,22 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.encabezado}>
+      <Encabezado/>
+      <Cuerpo/>
+      <Pie/>
+      <Pie2 opA='M' opD={'G'} opB={'E'} opC='F'/>
+      <Pie3 op1={'C'} op2={'R'} op3={'A'} op4={'P'}/>
+    </View>
+  );
+}
+export const Encabezado=()=>{
+  return(
+    <View style={styles.encabezado}>
         <Text style={styles.texto}>X</Text>
         <Text style={styles.texto}>Encabezado</Text>
         <Text style={styles.texto}>X</Text>
-      </View>
-      <View style={styles.cuerpo}>
-        <Text>Cuerpo</Text>
-      </View>
-      <View style={styles.pie}>
-        <Text>Pie</Text>
-      </View>
     </View>
-  );
+  )
+}
+
+export const Login=()=>{
+  return(
+    <View>
+      <Text>Username...</Text>
+      <TextInput placeholder='Username'/>
+      <Text>Password...</Text>
+      <TextInput placeholder='Password' />
+      <Button title='Login' />
+      <Button title='Cancel'/>
+    </View>
+  )
+}
+
+export const Cuerpo=()=>{
+  return(
+    <View style={styles.cuerpo}>
+        <Text style={styles.texto}>Cuerpo</Text>
+        <Login/>
+    </View>
+  )
+}
+
+export const Pie=()=>{
+  //aqui podria tener varias operaciones sin problemas 
+  return(
+    <View style={styles.pie}>
+        <Text style={styles.texto}>X</Text>
+        <Text style={styles.texto}>X</Text>
+        <Text style={styles.texto}>X</Text>
+        <Text style={styles.texto}>X</Text>
+    </View>
+  )
+}
+
+export const Pie2=(props)=>{
+  return(
+    <View style={styles.pie}>
+        <Text style={styles.texto}>{props.opA}</Text>
+        <Text style={styles.texto}>{props.opB}</Text>
+        <Text style={styles.texto}>{props.opC}</Text>
+        <Text style={styles.texto}>{props.opD}</Text>
+    </View>
+  )
+}
+
+export const Pie3=({op1, op2, op3,op4})=>{
+  //aqui podria tener varias operaciones sin problemas 
+  return(
+    <View style={styles.pie}>
+        <Text style={styles.texto}>{op1}</Text>
+        <Text style={styles.texto}>{op2}</Text>
+        <Text style={styles.texto}>{op3}</Text>
+        <Text style={styles.texto}>{op4}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -43,6 +103,9 @@ const styles = StyleSheet.create({
   },
   pie:{
     flex:1,
+    flexDirection:'row',
+    justifyContent:'space-evenly',
+    alignItems:'center',
     backgroundColor:'#227edb'
   }
 });

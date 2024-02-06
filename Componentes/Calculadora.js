a1aba044835844f97577710f5c7dcc2b0e20f2f0
LMendoza70 <luis.mendoza@uthh.edu.mx>
2024-02-06 12:03:33 -0600

    06/02/2024

---
+++ Componentes/Calculadora.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const Calculadora = () => {
+  return (
+    <View>
+      <Text>Calculadora</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+//snipet rnfcse
+
+rnf
